dc27cea56c6bbd247122f99e878bf30356dc9778
JD Tadlock <jt7903@gmail.com>
2024-08-13 11:07:15 -0400

    set up the client/server model

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const db = require('./config/connection');
 const api_routes = require('./routes/api_routes');
@@ -6,11 +7,19 @@ const api_routes = require('./routes/api_routes');
 const app = express();
 const PORT = 3333;
 
+// Create a GET route for every file inside of client
+app.use(express.static('../client'));
+
 // Add the JSON middleware / Allow JSON to be attached to req.body
 app.use(express.json());
 
 // Load our routes
 app.use('/api', api_routes);
+
+// Send back the index.html file for all other requests/routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
 db.once('open', () => {
   console.log('DB connection established');
